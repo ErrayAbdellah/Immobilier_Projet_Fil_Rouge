25b7afd31a4980ddb49c0f490f1ea6244df81400
ErrayAbdellah <mahdiabdllah121@gmail.com>
2023-04-13 15:39:49 +0000

    add style and create post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,7 @@ use App\Models\Type;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -52,6 +53,7 @@ class PostController extends Controller
             'space' => 'required',
             'price' => 'required',
             'outdoor_features' => 'array', 
+            
         ]);
 
         $post = new Post();
@@ -74,6 +76,7 @@ class PostController extends Controller
         foreach ($images as $image) {
             $filename = $image->getClientOriginalName();
             $path = $image->store('images'); // assuming you want to store the images in the public/images directory
+            $image->move(public_path('image_post'),$filename);
             
             Image::create([
                 'filename' => $filename,
@@ -128,31 +131,111 @@ class PostController extends Controller
      */
     public function update(Request $request,$id)
     {
+        // $validatedData = $request->validate([
+        //     'title' => 'required',
+        //     'description' => 'required',
+        //     'post_type' => 'required',
+        //     'outdoor_features' => 'array', 
+        // ]);
+    
+        // $post = Post::find($id); 
+    
+        // if (!$post) {
+        //     return response()->json(['error' => 'Post not found'], 404); 
+
+        //     return redirect()->back()->with('error', 'Post not found'); 
+        // }
+    
+        // // Update the post attributes
+        // $post->title = $validatedData['title'];
+        // $post->description = $validatedData['description'];
+        // $post->type_id = $validatedData['post_type'];
+        // $post->save();
+    
+        // // Sync the outdoor features
+        // $post->outdoorfeature()->sync($validatedData['outdoor_features']);
+
+        // $images = $request->file('images');
+        // if ($images) {
+        //     foreach ($images as $image) {
+        //         $filename = $image->getClientOriginalName();
+        //         $path = $image->store('images'); // Assuming you want to store the images in the public/images directory
+                
+        //         Image::create([
+        //             'filename' => $filename,
+        //             'path' => $path,
+        //             'post_id'=> $post->id,
+        //         ]);
+        //     }
+        // }
+    
+        // return redirect()->route('post.edit', $id)->with('success', 'Post updated successfully!'); 
+
+        // ************************************************************************
         $validatedData = $request->validate([
             'title' => 'required',
             'description' => 'required',
             'post_type' => 'required',
+            'Bedrooms' => 'required',
+            'space' => 'required',
+            'price' => 'required',
             'outdoor_features' => 'array', 
         ]);
     
-        $post = Post::find($id); 
-    
-        if (!$post) {
-            return response()->json(['error' => 'Post not found'], 404); 
-
-            return redirect()->back()->with('error', 'Post not found'); 
-        }
-    
-        // Update the post attributes
+        $post = Post::findOrFail($id); // Find the post by its ID
         $post->title = $validatedData['title'];
         $post->description = $validatedData['description'];
+        $post->Bedrooms = $validatedData['Bedrooms'];
+        $post->space = $validatedData['space'];
+        $post->price = $validatedData['price'];
         $post->type_id = $validatedData['post_type'];
-        $post->save();
+        // Update other fields as needed
+        
+        $post->outdoorfeature()->sync($validatedData['outdoor_features']); // Sync outdoor features relationship
+        
+        $post->save(); // Save the updated post
     
-        // Sync the outdoor features
-        $post->outdoorfeature()->sync($validatedData['outdoor_features']);
+        // Handle image updates, assuming you have an 'images' input field for file uploads
+        // $images = $request->file('images');
+        // if ($images) {
+        //     foreach ($images as $image) {
+        //         $filename = $image->getClientOriginalName();
+        //         $path = $image->store('public/image_post'); // Store the image in public/image_post directory
+        //         $path = str_replace('public', 'storage', $path); // Update the file path to use the storage symlink
+        //         $image->move(public_path('image_post'), $filename); // Move the image to public/image_post directory
+        //         Image::create([
+        //             'filename' => $filename,
+        //             'path' => $path,
+        //             'post_id'=> $post->id,
+        //         ]);
+        //     }
+        // }
+        $images = $request->file('images');
+        foreach ($images as $image) {
+            $filename = $image->getClientOriginalName();
+            $path = $image->store('images'); // assuming you want to store the images in the public/images directory
+            $image->move(public_path('image_post'),$filename);
+            
+            Image::create([
+                'filename' => $filename,
+                'path' => $path,
+                'post_id'=> $post->id,
+            ]);
+        }
+        // Update existing images
+        // $existingImages = Image::where('post_id', $post->id)->get(); // Retrieve existing images
+        // if ($existingImages) {
+        //     foreach ($existingImages as $existingImage) {
+        //         $existingImagePath = str_replace('storage', 'public', $existingImage->path); // Update the existing image path to use public directory
+        //         if (Storage::exists($existingImagePath)) {
+        //             Storage::delete($existingImagePath); // Delete the existing image file from storage
+        //         }
+        //         $existingImage->delete(); // Delete the existing image record from database
+        //     }
+        // }
     
-        return redirect()->route('post.edit', $id)->with('success', 'Post updated successfully!'); 
+        return redirect()->route('post.edit', $post->id)->with('success', 'Post updated successfully!');
+        // ************************************************************************
     }
 
     /**
@@ -162,6 +245,18 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
+    {
+        $post = Post::find($id); 
+
+        if (!$post) {
+            return response()->json(['error' => 'Post not found'], 404); 
+        }
+        
+        $post->delete();
+
+        return response()->json(['message' => 'Post deleted successfully']); 
+    }
+    public function destroyIm($id)
     {
         $post = Post::find($id); 
 
