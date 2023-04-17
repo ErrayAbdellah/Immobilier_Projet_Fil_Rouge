5dad6aa1b494ca475f854c52a0feddb7ae0e7a2c
ErrayAbdellah <mahdiabdllah121@gmail.com>
2023-04-17 16:00:06 +0000

    style page buy

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,56 +36,58 @@ class HomeController extends Controller
     }
 
 
-    public function filterPost(Request $request){
+    public function filterPost(Request $request)
+    {
     // Retrieve all posts
-    $query = Post::query();
+        $query = Post::query();
 
-    // Filter by Property Type
-    if ($request->has('Filterpost_type')) {
-        $query->where('type_id', $request->input('Filterpost_type'));
+        // Filter by Property Type
+        if ($request->has('Filterpost_type')) {
+            $query->where('type_id', $request->input('Filterpost_type'));
+            
+        }
+
+        // Filter by Price Range
+        if ($request->has('filterMinPrice')) {
+            $query->where('price', '>=', $request->input('filterMinPrice'));
+        }
+        if ($request->has('filterMaxPrice')) {
+            $query->where('price', '<=', $request->input('filterMaxPrice'));
+        }
+
+        // Filter by Bedrooms
+        if ($request->has('filterNumBedrooms')) {
+            $query->where('Bedrooms', $request->input('filterNumBedrooms'));
+        }
+
+        // Filter by Outdoor Features
+        // if ($request->has('filterOutdoor_features')) {
+        //     $outdoorFeatureIds = $request->input('filterOutdoor_features');
+        //     $query->whereHas('outdoorfeature', function ($query) use ($outdoorFeatureIds) {
+        //         $query->whereIn('outdoorfeature_id', $outdoorFeatureIds);
+        //     });
+        // }
+
+        $posts = $query->get();
         
-    }
 
-    // Filter by Price Range
-    if ($request->has('filterMinPrice')) {
-        $query->where('price', '>=', $request->input('filterMinPrice'));
-    }
-    if ($request->has('filterMaxPrice')) {
-        $query->where('price', '<=', $request->input('filterMaxPrice'));
-    }
-
-    // Filter by Bedrooms
-    if ($request->has('filterNumBedrooms')) {
-        $query->where('Bedrooms', $request->input('filterNumBedrooms'));
-    }
-
-    // Filter by Outdoor Features
-    // if ($request->has('filterOutdoor_features')) {
-    //     $outdoorFeatureIds = $request->input('filterOutdoor_features');
-    //     $query->whereHas('outdoorfeature', function ($query) use ($outdoorFeatureIds) {
-    //         $query->whereIn('outdoorfeature_id', $outdoorFeatureIds);
-    //     });
-    // }
-
-    $posts = $query->get();
-    
-
-    $images = Image::with('post')->get();
-        $types = Type::all();
-        $outdoorFeatures = Outdoorfeature::all();
-    return view('Home.product', compact('posts','images','types','outdoorFeatures'));
+        $images = Image::with('post')->get();
+            $types = Type::all();
+            $outdoorFeatures = Outdoorfeature::all();
+        return view('Home.product', compact('posts','images','types','outdoorFeatures'));
 
     }
 
     public function createBuypage(Request $request, $id){
 
-        $post = Post::find($id);
+        $post = Post::with('type','outdoorfeature')->where('id','=',$id)->get()->first();
+        // dd($post);
         $images = Image::with('post')->where('post_id','=',$id)->get();
         $comments = Comment::with('post')->where('post_id','=',$id)->get();
         $users = User::with('comment')->get();
         // dd($users);
-        $types = Type::all();
+        // $types = Type::all();
         $outdoorFeatures = Outdoorfeature::all();
-        return view('Home.buy-page',compact('post','images','types','outdoorFeatures','comments','users'));
+        return view('Home.buy-page',compact('post','images','outdoorFeatures','comments','users'));
     }
 }
--- resources/views/Home/buy-page.blade.php
+++ resources/views/Home/buy-page.blade.php
@@ -8,22 +8,43 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
 
-
+<style>
+.scrollComement{
+    
+    ::-webkit-scrollbar-track{
+            background: #ffffff6b
+        }
+    ::-webkit-scrollbar-thumb{
+        /* background: linear-gradient(to right,); */
+        background: #83838389;
+        border-radius:20px;
+    }
+    ::-webkit-scrollbar-thumb:hover{
+        background: #000000da;
+        /* background: linear-gradient(to right,#02a1dbda,#a8cf45ce); */
+    }
+    ::-webkit-scrollbar{
+        width: 7px;
+        height: 4px;
+    }
+}
+</style>
 </head>
 <body>
     <div class="container mx-auto">
         <div class="">
-            <div class="flex flex-col md:flex-row md:items-center">
+            <div class="flex flex-col md:flex-row md:items-center ">
                 <div class="mt-4 md:mt-0 md:w-1/2 md:pl-8">
                     <h2 class="text-xl font-bold text-gray-800 mb-2">{{$post->title}}</h2>
-                    <p class="text-gray-500">{{$post->title}} </p>
+                    <p class="text-gray-500">{{$post->description}}</p>
                     <p class="text-lg font-semibold text-gray-700 mb-4">{{$post->price}} MAD</p>                    
                     {{-- description --}}
                     <div class="m-4 mt-7">
                         
                     </div>
                 </div>
-                <div class="md:w-2/5">
+                {{-- images --}}
+                <div class="md:w-2/5 h-80">
                     {{-- <img src="https://via.placeholder.com/350" alt="Product Image" class="w-full object-cover object-center"> --}}
                     
                     <div id="controls-carousel" class="relative w-full" data-carousel="static">
@@ -35,8 +56,6 @@
                                             <img src="{{ asset('image_post/'.$image->filename) }}" alt="Just a flower" class="  object-fill  rounded-2xl">
                                         </div>
                                     @endforeach
-                                
-                                
                               </div>
                             <!-- ***************************** -->
                         </div>
@@ -56,67 +75,93 @@
                     </div>
 
                     {{-- *************************************************** --}}
-                    <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-                       
-                        <div class="flex flex-col items-center pb-10">
-                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('image_post/'.$image->filename) }}" alt="Bonnie image"/>
-                            <h5 class="mb-1 text-xl font-medium text-gray-900">Bonnie Green</h5>
-                            <span class="text-sm text-gray-500">Visual Designer</span>
-                        </div>
-                    </div>
                 </div>
-                
             </div>            
             
-        {{--   <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
-                <p class="font-normal text-gray-700 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            </a> --}}
-            {{-- show comments --}}
             
-            <div class="bg-gray-400 ">
-                <h1 class="my-5 text-2xl p-5 ">Comments</h1>
-                @foreach ($comments as $comment) 
-                    @foreach ($users as $user)
-                    @if ($comment->user_id == $user->id)
-                        <article class="sm:w-[40rem] bg-gray-200 rounded-2xl p-2 mt-4 mb-4 ">
-                            <div class="flex items-center mt-6 space-x-4">
-                                <img class="w-auto text-sm h-10 rounded-full" src="https://images.pexels.com/photos/678783/pexels-photo-678783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-                                <div class="space-y-1 font-medium">
-                                    <p>{{$user->name}}</p>
-                                </div>
-                            </div>
-                            <div>
-                                <footer class="mb-5  text-gray-500"><p><time datetime="2017-03-03 19:00">     {{ $comment->created_at}}</time></p></footer>
-                                <p class="mb-2 text-gray-500">{{ $comment->description}}</p>
-                            </div>
-                            <a href="#" class="block mb-5 text-sm font-medium text-blue-600 hover:underline">like</a> <p> 12 likes</p>
-                            
-                        </article>
-                        @endif
+          <div class="flex justify-around flex-wrap ">
+           
+            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow ">
+                <div>
+                    <h1>Bedrooms</h1>
+                    <h5 class="w-24 p-2 mt-2 text-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">{{$post->Bedrooms}}</h5>
+                </div>
+                <div>
+                    <h1>type</h1>
+                    <h5 class="w-24 p-2 mt-2 text-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">{{$post->type->name}}</h5>
+                </div>
+                <div>
+                    <h1>Outdoor Features</h1>
+                <div class="flex justify-around flex-wrap">
+                    @foreach ($post->outdoorfeature as $item)
+                    <h5 class="w-24 p-2 mt-2 text-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">{{$item->name}}</h5>
                     @endforeach
-                @endforeach
+                </div>
+                </div>
+                <div>
+                    <h1>Space</h1>
+                    <h5 class="w-24 p-2 mt-2 text-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">{{$post->space}}m²</h5>
+                </div>
             </div>
 
-            {{-- comment --}}
-            
-            <form action="{{route('comment.store')}}" method="POST" class="sm:w-[40rem]" >
-                @csrf
-                <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50">
-                    <div class="px-4 py-2 bg-white rounded-t-lg">
-                        <label for="comment" class="sr-only">Your comment</label>
-                        <textarea id="comment" name="commentPost" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0" placeholder="Write a comment..." required></textarea>
-                    </div>
-                    <div class="flex items-center justify-between px-3 py-2 border-t">
-                        <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200  hover:bg-blue-800">
-                            Post comment
-                        </button>
-                    </div>
-                    <input type="hidden" name="idPostComment" value="{{$post->id}}">
+                <div class="flex flex-col items-center pb-10 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow  ">
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('image_post/'.$image->filename) }}" alt="Bonnie image"/>
+                    <h5 class="mb-1 text-xl font-medium text-gray-900">Bonnie Green</h5>
+                    <span class="text-sm text-gray-500">Visual Designer</span>
                 </div>
-            </form>
-            <p class="ml-auto text-xs text-gray-500 ">Remember, contributions to this topic should follow our <a href="#" class="text-blue-600 hover:underline">Community Guidelines</a>.</p>
- 
+            
+          </div>
+                
+           
+            {{-- show comments --}}
+            
+            <div class=" flex justify-center flex-wrap ">
+                <div class="scrollComement">
+                    <h1 class="my-5 text-2xl p-5 ">Review</h1>
+                    <div class="h-44 overflow-scroll h-[50rem] ">
+                        @foreach ($comments as $comment) 
+                            @foreach ($users as $user)
+                            @if ($comment->user_id == $user->id)
+                                <article class="sm:w-[40rem] bg-gray-200 rounded-2xl p-2 mt-4 mb-4 ">
+                                    <div class="flex items-center mt-6 space-x-4">
+                                        <img class="w-auto text-sm h-10 rounded-full" src="https://images.pexels.com/photos/678783/pexels-photo-678783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                                        <div class="space-y-1 font-medium">
+                                            <p>{{$user->name}}</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <footer class="mb-5  text-gray-500"><p><time datetime="2017-03-03 19:00">{{ $comment->created_at}}</time></p></footer>
+                                        <p class="mb-2 text-gray-500">{{ $comment->description}}</p>
+                                    </div>
+                                    {{-- <a href="#" class="block mb-5 text-sm font-medium text-blue-600 hover:underline">like</a> <p> 12 likes</p> --}}
+                                </article>
+                                @endif
+                            @endforeach
+                        @endforeach
+                    </div>
+                
+            
+
+                    {{-- comment --}}
+                    
+                    <form action="{{route('comment.store')}}" method="POST" class="sm:w-[40rem]" >
+                        @csrf
+                        <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50">
+                            <div class="px-4 py-2 bg-white rounded-t-lg">
+                                <label for="comment" class="sr-only">Your comment</label>
+                                <textarea id="comment" name="commentPost" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0" placeholder="Write a comment..." required></textarea>
+                            </div>
+                            <div class="flex items-center justify-between px-3 py-2 border-t">
+                                <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200  hover:bg-blue-800">
+                                    Post comment
+                                </button>
+                            </div>
+                            <input type="hidden" name="idPostComment" value="{{$post->id}}">
+                        </div>
+                    </form>
+                    <p class="ml-auto text-xs text-gray-500 ">Remember, contributions to this topic should follow our <a href="#" class="text-blue-600 hover:underline">Community Guidelines</a>.</p>
+                </div>
+            </div>
         </div>
     </div>
 
